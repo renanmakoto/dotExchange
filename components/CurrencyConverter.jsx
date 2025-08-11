b4f4a8c1e31415ca3d27be93b968d6d09377451b
user <renanmakoto@outlook.com>
2025-08-11 08:11:13 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, TextInput, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { View, TextInput, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native'
 import axios from 'axios';
 
 export default function CurrencyConverter() {
