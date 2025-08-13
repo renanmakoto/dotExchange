5984b918ebe6906b07ad78d4cc4fd7f7eb2e3a59
user <renanmakoto@outlook.com>
2025-08-13 17:42:15 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import { View, TextInput, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import axios from "axios";
 import HistoryGraphic from "./HistoryGraphic"; // same folder
