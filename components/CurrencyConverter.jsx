483fa423c34b068f5a0b765640936c7f2d448a6e
user <renanmakoto@outlook.com>
2025-08-14 07:00:32 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -18,7 +18,7 @@ export default function CurrencyConverter() {
   const toBCBDate = (d) => {
     const dd = String(d.getDate()).padStart(2, "0")
     const mm = String(d.getMonth() + 1).padStart(2, "0")
-    const yyyy = d.getFullYear();
+    const yyyy = d.getFullYear()
     return `${mm}-${dd}-${yyyy}`; // BCB expects MM-DD-YYYY
   };
 
