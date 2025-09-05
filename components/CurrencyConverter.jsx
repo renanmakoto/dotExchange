4ce4f924580d1d73dc642b31d6004ec1d46e99a3
user <renanmakoto@outlook.com>
2025-09-05 08:23:08 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -70,7 +70,7 @@ function parseTimestampParts(utcString) {
   return { timeStr, dateStr };
 }
 
-// ---- date helpers ----
+//DATE HELPERS
 function mmddyyyy(d) {
   const mm = String(d.getMonth() + 1).padStart(2, '0');
   const dd = String(d.getDate()).padStart(2, '0');
