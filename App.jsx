46994cabbb995047dbae7d53785020f3769036fa
user <renanmakoto@outlook.com>
2025-09-08 08:37:12 -0400

    Clean code

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { View, StyleSheet } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
