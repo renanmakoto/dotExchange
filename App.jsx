ce689f089e208fb99e98306e2e607a4fc1660179
user <renanmakoto@outlook.com>
2025-09-11 14:57:20 -0400

    Clean code

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { View, StyleSheet, StatusBar, Platform } from 'react-native'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import CurrencyConverter from './components/CurrencyConverter';
+import CurrencyConverter from './components/CurrencyConverter'
 
 export default function App() {
   useEffect(() => {
