50a258f069c47a660205cb248802486dd007e184
user <renanmakoto@outlook.com>
2025-09-11 07:45:44 -0400

    Clean code

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, StyleSheet, StatusBar } from 'react-native'
-import Header from './components/Header';
+import Header from './components/Header'
 import Footer from './components/Footer';
 import CurrencyConverter from './components/CurrencyConverter';
 
