22fe0e9906390cccd6a9eec9c163c754b4485be4
user <renanmakoto@outlook.com>
2025-09-11 07:45:51 -0400

    Clean code

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, StyleSheet, StatusBar } from 'react-native'
 import Header from './components/Header'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
 import CurrencyConverter from './components/CurrencyConverter';
 
 export default function App() {
