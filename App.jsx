45aeb5665e4ea5ddf44af776ae5fdcee7587e88d
user <renanmakoto@outlook.com>
2025-09-11 14:56:57 -0400

    Clean code

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import { View, StyleSheet, StatusBar, Platform } from 'react-native';
 import Header from './components/Header';
 import Footer from './components/Footer';
