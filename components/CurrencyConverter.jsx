dd18aff7eb905f29f58eed9d26e225cad29af635
user <renanmakoto@outlook.com>
2025-09-23 07:53:11 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -345,7 +345,7 @@ async function fetchMonthlySeries(base, quote) {
           series.push(usdPerBtc * usdToQuote)
         } else if (quote === 'BTC' && base !== 'USD') {
           const { rate: baseToUsd } = await fetchFiatRate(base, 'USD', dayStr)
-          series.push(baseToUsd / usdPerBtc);
+          series.push(baseToUsd / usdPerBtc)
         } else if (base === 'BTC' && quote === 'USD') {
           series.push(usdPerBtc);
         } else if (base === 'USD' && quote === 'BTC') {
