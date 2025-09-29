2db84556a99c51f3149707221885fca59b444443
user <renanmakoto@outlook.com>
2025-09-29 07:55:56 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -436,7 +436,7 @@ export default function CurrencyConverter() {
       const { rate, timestampUTC, hasTime, source } = await fetchAnyRate(from, to)
       const converted = amt * rate
 
-      const left = `${formatAmount(amt, from)} ${from}`;
+      const left = `${formatAmount(amt, from)} ${from}`
       const right = `${formatAmount(converted, to)} ${to}`;
       setConvertedText(`${left} = ${right}`);
       setRateTimestampUTC(timestampUTC);
