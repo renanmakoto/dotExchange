53847e0557569b1b99c146dc0dbbe6cccd657d07
user <renanmakoto@outlook.com>
2025-09-29 07:47:54 -0400

    Clean code

--- components/CurrencyConverter.jsx
+++ components/CurrencyConverter.jsx
@@ -412,7 +412,7 @@ export default function CurrencyConverter() {
     setErrorMsg('')
     setConvertedText('')
     setRateTimestampUTC('')
-    setRateHasTime(false);
+    setRateHasTime(false)
     setGraphData([]);
     setGraphLabels([]);
 
